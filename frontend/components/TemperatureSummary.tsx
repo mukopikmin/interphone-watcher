7b38c2e9616549a6d2f919f811ffe592089b746b
Yusuke Mukai <mukopikmin@gmail.com>
2021-05-07 23:51:02 +0900

    Add eslint and prettier

--- frontend/components/TemperatureSummary.tsx
+++ frontend/components/TemperatureSummary.tsx
@@ -1,4 +1,3 @@
-import { useTemperatureDevices } from '../hooks/temperature'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
