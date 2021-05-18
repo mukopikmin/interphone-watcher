95c30eb6cf7a1bd0818e37f0f29efa8c3ed62a71
Yusuke Mukai <mukopikmin@gmail.com>
2021-05-19 02:01:09 +0900

    Aggeragated envi var definition

--- device/temperature.py
+++ device/temperature.py
@@ -1,39 +1,31 @@
 import datetime
-import logging
 import os
-import random
-import ssl
 import time
-import wave
 import time
 import json
-
-import numpy as np
-import jwt
-import paho.mqtt.client as mqtt
-import iotcore
 import seeed_dht
-
 from dotenv import load_dotenv
+import iotcore
 
 load_dotenv()
 
 config = {}
 
+DHT_SENSOR = int(os.environ.get("DHT_SENSOR") or "5")
+REGION = os.environ.get("REGION")
+DEVICE_ID = os.environ.get("DEVICE_ID")
+PROJECT_ID = os.environ.get("PROJECT_ID")
+TEMPERATURE_REGISTRY_ID = os.environ.get("TEMPERATURE_REGISTRY_ID")
+
 
 def main(
     algorithm,
     ca_certs,
-    cloud_region,
-    device_id,
     jwt_expires_minutes,
-    listen_dur,
     message_type,
     mqtt_bridge_hostname,
     mqtt_bridge_port,
     private_key_file,
-    project_id,
-    registry_id,
 ):
     """Connects a device, sends data, and receives data."""
     global config
@@ -41,15 +33,15 @@ def main(
     # Publish to the events or state topic based on the flag.
     sub_topic = "events" if message_type == "event" else "state"
 
-    mqtt_topic = "/devices/{}/{}/temperature".format(device_id, sub_topic)
+    mqtt_topic = "/devices/{}/{}/temperature".format(DEVICE_ID, sub_topic)
 
     jwt_iat = datetime.datetime.utcnow()
     jwt_exp_mins = jwt_expires_minutes
     client = iotcore.get_client(
-        project_id,
-        cloud_region,
-        registry_id,
-        device_id,
+        PROJECT_ID,
+        REGION,
+        TEMPERATURE_REGISTRY_ID,
+        DEVICE_ID,
         private_key_file,
         algorithm,
         ca_certs,
@@ -57,14 +49,14 @@ def main(
         mqtt_bridge_port,
     )
 
-    sensor = seeed_dht.DHT("11", 12)
+    sensor = seeed_dht.DHT("11", DHT_SENSOR)
 
     while True:
         client.loop()
 
         humi, temp = sensor.read()
         data = {
-            "deviceId": device_id,
+            "deviceId": DEVICE_ID,
             "timestamp": datetime.datetime.now().isoformat(),
             "humidity": humi,
             "temperature": temp,
@@ -79,10 +71,10 @@ def main(
             client.loop()
             client.disconnect()
             client = iotcore.get_client(
-                project_id,
-                cloud_region,
-                registry_id,
-                device_id,
+                PROJECT_ID,
+                REGION,
+                TEMPERATURE_REGISTRY_ID,
+                DEVICE_ID,
                 private_key_file,
                 algorithm,
                 ca_certs,
@@ -92,7 +84,7 @@ def main(
 
         client.publish(mqtt_topic, payload, qos=1)
 
-        for i in range(0, 60):
+        for _ in range(0, 60):
             time.sleep(1)
             client.loop()
 
@@ -103,28 +95,18 @@ def main(
 if __name__ == "__main__":
     algorithm = "RS256"
     ca_certs = "keys/roots.pem"
-    cloud_region = os.environ.get("REGION")
-    device_id = os.environ.get("DEVICE_ID")
     jwt_expires_minutes = 20
-    listen_dur = 60
     message_type = "event"
     mqtt_bridge_hostname = "mqtt.googleapis.com"
     mqtt_bridge_port = 8883
     private_key_file = "keys/rsa_private.pem"
-    project_id = os.environ.get("PROJECT_ID")
-    registry_id = os.environ.get("TEMPERATURE_REGISTRY_ID")
 
     main(
         algorithm,
         ca_certs,
-        cloud_region,
-        device_id,
         jwt_expires_minutes,
-        listen_dur,
         message_type,
         mqtt_bridge_hostname,
         mqtt_bridge_port,
         private_key_file,
-        project_id,
-        registry_id,
     )
